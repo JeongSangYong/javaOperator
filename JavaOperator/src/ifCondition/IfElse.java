b0c86aa6b5ce5069bc27abf5950169522c4b12ef
SuperClass <SuperClass>
2015-07-08 15:18:45 +0900

    2015.07.08. 3일차 종료(if-else까지)

---
+++ JavaOperator/src/ifCondition/IfElse.java
@@ -0,0 +1,19 @@
+package ifCondition;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int x = 1, y = 2;
+		if ((x==1) && (y==1))  // && 엔퍼센트, and연산자
+		{
+			System.out.println("x는 1이면서 y도 2이다.");
+		}
+		else if ((x==3) || (y==3)) // || 컬럼, or연산자
+		{	
+			System.out.println("x는 1이거나 y는 2이다.");
+		}
+		else
+		{
+			System.out.println("해당사항이 없습니다.");
+		}
+	}
+}
